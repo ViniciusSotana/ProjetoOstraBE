93914a8ab6eb335d9e206679b50f5be417504fe3
ViniciusSotana <vinysotana@gmail.com>
2025-12-22 15:58:00 -0300

    initial commit

---
+++ src/main/java/dev/projetoostra/projetoostra/ProjetoOstraApplication.java
@@ -0,0 +1,13 @@
+package dev.projetoostra.projetoostra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOstraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoOstraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/projetoostra/projetoostra/ProjetoOstraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projetoostra.projetoostra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOstraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
